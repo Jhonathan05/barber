0e53c20b31739e254e2cd4ae33f9cdc92da19283
Jhonathan <jacireno@hotmail.com>
2019-02-17 11:15:33 -0500

    Inicio Animacion de carga

---
+++ indexñ.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <meta http-equiv="refresh" content="2;url=home.php">
+    <title>Nanoware</title>
+</head>
+<body>
+    <div class="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</body>
+</html>
